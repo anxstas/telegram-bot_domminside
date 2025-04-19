7e9acfcbe01163ffc0ca43d35dd1e5fd5c296ebe
anxstas <1@advocats.org>
2025-04-19 11:46:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1251,6 +1251,12 @@ def finish_chat(message):
 
     logging.info("Бот запущен")
 
+@app.route(f"/bot{TELEGRAM_TOKEN}", methods=["POST"])
+def webhook():
+    if request.method == "POST":
+        update = telebot.types.Update.de_json(request.get_json())
+        bot.process_new_updates([update])
+    return "ok", 200
 
 if __name__ == "__main__":
     print(">>> Устанавливаем webhook:", f"{WEBHOOK_URL}/bot{TELEGRAM_TOKEN}")
